3019ffe360fc6e0684bcb455848231d799bd310d
Jaime <jaivalz@yahoo.com>
2016-03-08 19:56:12 +0100

    Created user-task association

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-
+  resources :tasks
   devise_for :users
+  get 'pages/home'
+
   root 'pages#home'
 
-  get 'about' => "pages#about"
+  get 'about' => 'pages#about'
 end
