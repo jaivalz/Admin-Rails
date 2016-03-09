f06a0fa68e0cf34540ef3d0d3c56d6832abf5a7b
Jaime <jaivalz@yahoo.com>
2016-03-09 16:27:41 +0100

    Added actions, added test, improved look, added link detection with autohtml and added validations

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,13 @@
 class PagesController < ApplicationController
   def home
+  	if current_user
+  		redirect_to tasks_path
+  	end
   end
 
-  def about    
+  def about
   end
 
+  def test
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,19 @@
 Rails.application.routes.draw do
-  resources :tasks
+  resources :tasks do
+    member do
+      put :change
+    end
+  end
   devise_for :users
-  get 'pages/home'
 
+  get 'test' => 'pages#test'
+
+  # The priority is based upon order of creation: first created -> highest priority.
+  # See how all your routes lay out with "rake routes".
+
+  # You can have the root of your site routed with "root"
   root 'pages#home'
 
   get 'about' => 'pages#about'
+
 end
